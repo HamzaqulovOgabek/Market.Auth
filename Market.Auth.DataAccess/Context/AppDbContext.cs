8640f40d508f9bac92ab2d78e8c6a81bc6932b15
Ogabek <hamzaqulovogabek@gmail.com>
2024-09-24 12:12:13 +0500

    Configured User Name and Email To Be Unique

--- Market.Auth.DataAccess/Context/AppDbContext.cs
+++ Market.Auth.DataAccess/Context/AppDbContext.cs
@@ -7,8 +7,8 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions options) : base(options)
     {
-        
-    }   
+
+    }
     public DbSet<Permission> Permissions { get; set; }
     public DbSet<Role> Roles { get; set; }
     public DbSet<RolePermission> RolePermissions { get; set; }
@@ -16,4 +16,18 @@ public class AppDbContext : DbContext
     public DbSet<UserDevice> UserDevices { get; set; }
     public DbSet<UserRole> UserRoles { get; set; }
     public DbSet<UserToken> UserTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(options =>
+        {
+            options.HasIndex(u => u.Email)
+            .IsUnique();
+
+            options.HasIndex(u => u.UserName)
+            .IsUnique();
+        });
+    }
 }
