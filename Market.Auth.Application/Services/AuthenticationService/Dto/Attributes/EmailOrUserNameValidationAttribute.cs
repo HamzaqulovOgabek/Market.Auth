5a1a631938f02e5742c49c1fee82f13af23a2535
Ogabek <hamzaqulovogabek@gmail.com>
2024-09-26 11:30:51 +0500

    Added Optional Login With Username

---
+++ Market.Auth.Application/Services/AuthenticationService/Dto/Attributes/EmailOrUserNameValidationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Market.Auth.Application.Services.AuthenticationService
+{ 
+    public class EmailOrUserNameValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var userLoginDto = validationContext.ObjectInstance as UserLoginDto;
+            
+            if(string.IsNullOrWhiteSpace(userLoginDto.EmailOrUsername))
+                return new ValidationResult("Email or username is required");
+
+            bool isEmail = userLoginDto.IsValidEmail(userLoginDto.EmailOrUsername);
+            bool isUsername = userLoginDto.IsValidUserName(userLoginDto.EmailOrUsername);
+
+            if(!isEmail && !isUsername)
+                return new ValidationResult("Invalid email or username");
+
+            return ValidationResult.Success;
+        }
+    }
+}
